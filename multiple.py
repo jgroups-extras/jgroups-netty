cc252e38d57d13bb9b0693a101425db9ff71a507
baizel <baizelmathew@yahoo.co.in>
2020-05-20 22:39:13 +0100

    Add script for load testing

---
+++ multiple.py
@@ -0,0 +1,21 @@
+import subprocess
+from sys import argv
+
+"""
+Very quick and dirty script to run multiple instances of UPerf for load testing
+"""
+
+_, props, instances = argv
+print(props, instances)
+all_pid = []
+for i in range(int(instances)):
+    # Host nodes must be up or everything will have merge view otherwise need to add a delay in after each process
+    process = subprocess.Popen(["java", "-classpath", "./target/jgroups-netty-1.0-SNAPSHOT.jar:target/dependency/*",
+                                "org.jgroups.tests.perf.UPerf", "-props", props, "-nohup"])
+    all_pid.append(process)
+
+try:
+    t = input("any Button to exit")
+finally:
+    for i in all_pid:
+        i.kill()
